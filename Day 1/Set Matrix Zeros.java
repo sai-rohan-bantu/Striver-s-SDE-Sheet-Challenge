d37583f7e89da6062809691f06f088cf93900ac2
Sai Rohan Bantu <110375419+sai-rohan-bantu@users.noreply.github.com>
2022-08-01 13:47:48 +0530

    Update Set Matrix Zeros.java

--- Day 1/Set Matrix Zeros.java
+++ Day 1/Set Matrix Zeros.java
@@ -1,1 +1,30 @@
-
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        int r=matrix.length;int c=matrix[0].length;
+        int[] row=new int[r];
+        Arrays.fill(row,-1);
+        int[] col=new int[c];
+        Arrays.fill(col,-1);
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=0;
+                    col[j]=0;
+                }
+            }
+        }
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(row[i]==0 || col[j]==0)
+                    matrix[i][j]=0;
+            }
+        }
+    }
+}
